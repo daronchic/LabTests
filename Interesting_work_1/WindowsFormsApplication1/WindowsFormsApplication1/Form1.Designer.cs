43e45c60072aa51da4669bfd31f1341241f70b74
daronchic <daronchic@yandex.ru>
2014-05-21 16:05:01 +0400

    New button. no sence

--- Interesting_work_1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Interesting_work_1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -160,9 +160,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 206);
+            this.button1.Location = new System.Drawing.Point(12, 206);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(186, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
